02009ab23009c32e0c7660320cf33ef0b89066c3
Robin Laerm <Robin.Laerm@xitaso.com>
2026-05-08 10:56:16 +0200

    deactivate web search test (#890)
    * deactivate web search test
    
    * use test.fixme and add ticket number

--- apps/dialog/e2e/tests/external-services/web-search.test.ts
+++ apps/dialog/e2e/tests/external-services/web-search.test.ts
@@ -4,16 +4,21 @@ import { sendMessage } from '../../utils/chat';
 
 test.use({ storageState: AUTH_FILES.teacher });
 
-test('should successfully perform web search', { tag: '@external-services' }, async ({ page }) => {
-  await page.goto('/');
+// Test is flaky --> TD-1226
+test.fixme(
+  'should successfully perform web search',
+  { tag: '@external-services' },
+  async ({ page }) => {
+    await page.goto('/');
 
-  const websearchToggle = page.getByRole('button', { name: 'Internetquellen', exact: true });
+    const websearchToggle = page.getByRole('button', { name: 'Internetquellen', exact: true });
 
-  // send a message that does not require web search
-  await sendMessage(page, 'Hallo');
-  await expect(websearchToggle).not.toBeVisible();
+    // send a message that does not require web search
+    await sendMessage(page, 'Hallo');
+    await expect(websearchToggle).not.toBeVisible();
 
-  // send a message that requires web search
-  await sendMessage(page, 'Wie ist aktuell das Wetter in Augsburg?');
-  await expect(websearchToggle).toBeVisible();
-});
+    // send a message that requires web search
+    await sendMessage(page, 'Wie ist aktuell das Wetter in Augsburg?');
+    await expect(websearchToggle).toBeVisible();
+  },
+);
