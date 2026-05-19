92beefd60f7cfdf3c9549d50f2534f0b595841ce
Robin Laerm <Robin.Laerm@xitaso.com>
2026-05-19 11:59:35 +0200

    reactivate web search test (#947)

--- apps/chat-bot/e2e/tests/external-services/web-search.test.ts
+++ apps/chat-bot/e2e/tests/external-services/web-search.test.ts
@@ -4,21 +4,16 @@ import { sendMessage } from '../../utils/chat';
 
 test.use({ storageState: AUTH_FILES.teacher });
 
-// Test is flaky --> TD-1226
-test.fixme(
-  'should successfully perform web search',
-  { tag: '@external-services' },
-  async ({ page }) => {
-    await page.goto('/');
+test('should successfully perform web search', { tag: '@external-services' }, async ({ page }) => {
+  await page.goto('/');
 
-    const websearchToggle = page.getByRole('button', { name: 'Internetquellen', exact: true });
+  const websearchToggle = page.getByRole('button', { name: 'Internetquellen', exact: true });
 
-    // send a message that does not require web search
-    await sendMessage(page, 'Hallo');
-    await expect(websearchToggle).not.toBeVisible();
+  // send a message that does not require web search
+  await sendMessage(page, 'Hallo');
+  await expect(websearchToggle).not.toBeVisible();
 
-    // send a message that requires web search
-    await sendMessage(page, 'Wie ist aktuell das Wetter in Augsburg?');
-    await expect(websearchToggle).toBeVisible();
-  },
-);
+  // send a message that requires web search
+  await sendMessage(page, 'Wie ist aktuell das Wetter in Augsburg?');
+  await expect(websearchToggle).toBeVisible();
+});
