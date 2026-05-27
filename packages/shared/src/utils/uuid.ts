e6ee476af4f4288014e4cc6e6f91728bac9300f6
Robin Laerm <Robin.Laerm@xitaso.com>
2026-05-27 14:40:52 +0200

    replace custom uuid logic (#972)

--- packages/shared/src/utils/uuid.ts
+++ packages/shared/src/utils/uuid.ts
@@ -1,12 +1,9 @@
+import { validate as validateUUID, v4 as generateUUIDv4 } from 'uuid';
+
 export function generateUUID(): string {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
-    const r = (Math.random() * 16) | 0;
-    const v = c === 'x' ? r : (r & 0x3) | 0x8;
-    return v.toString(16);
-  });
+  return generateUUIDv4();
 }
 
 export function isUUID(uuid: string): boolean {
-  const uuidRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
-  return uuidRegex.test(uuid);
+  return validateUUID(uuid);
 }
